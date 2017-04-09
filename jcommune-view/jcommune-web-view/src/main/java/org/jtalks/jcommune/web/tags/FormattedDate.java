d933f5a5ad1390d68dfad13e78f58c0bcf4c6543
ghp-mac <pakkeunhee@gmail.com>
2017-04-09 23:48:30 +0900

    [#1] 한국어용 날짜 format 적용

--- jcommune-view/jcommune-web-view/src/main/java/org/jtalks/jcommune/web/tags/FormattedDate.java
+++ jcommune-view/jcommune-web-view/src/main/java/org/jtalks/jcommune/web/tags/FormattedDate.java
@@ -14,128 +14,136 @@
  */
 package org.jtalks.jcommune.web.tags;
 
-
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.contrib.jsptag.FormatTag;
+import org.jtalks.jcommune.model.entity.Language;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
+
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 /**
- * This tag replaces the existing Joda Time format tag to take
- * into account the local time offset set in cookies.
- * Although the library has out-of-box timezone support,
- * we cannot use it since we don't know the timezone.
- * Automatic timezone detection is a complicated task:
- * - ip-based detection is inaccurate for some regions
- * - bare time offset cannot be properly mapped on the timezone,
- * 'cause timezones have different DST settings
+ * This tag replaces the existing Joda Time format tag to take into account the local time offset set in cookies. Although the library has out-of-box timezone support, we cannot use it since we don't
+ * know the timezone. Automatic timezone detection is a complicated task: - ip-based detection is inaccurate for some regions - bare time offset cannot be properly mapped on the timezone, 'cause
+ * timezones have different DST settings
  * <p/>
- * We also could ask the user himself for the timezone, but it's
- * as boring, as irritating.
+ * We also could ask the user himself for the timezone, but it's as boring, as irritating.
  *
  * @author Evgeniy Naumenko
  */
 public class FormattedDate extends FormatTag {
 
-    /**
-     * Serializable class should define it
-     */
-    private static final long serialVersionUID = 34588L;
+	/**
+	 * Serializable class should define it
+	 */
+	private static final long serialVersionUID = 34588L;
 
-    public static final String GMT_COOKIE_NAME = "GMT";
+	public static final String GMT_COOKIE_NAME = "GMT";
 
-    /**
-     * Example: 01 Jan 2011 05:13
-     * Localized month names are to be inserted by magic
-     */
-    public static final String DEFAULT_DATE_FORMAT_PATTERN = "dd MMM yyyy HH:mm";
+	/**
+	 * Example: 01 Jan 2011 05:13 Localized month names are to be inserted by magic
+	 */
+	public static final String DEFAULT_DATE_FORMAT_PATTERN = "dd MMM yyyy HH:mm";
+	public static final String DEFAULT_DATE_FORMAT_PATTERN_KOREAN = "yyyy-MM-dd HH:mm";
 
-    private long offset = DEFAULT_OFFSET;
+	private long offset = DEFAULT_OFFSET;
 
-    public static final int DEFAULT_OFFSET = 0;
+	public static final int DEFAULT_OFFSET = 0;
 
-    public FormattedDate() {
-        super();
-        // override parent default
-        pattern = DEFAULT_DATE_FORMAT_PATTERN;
-    }
+	public FormattedDate() {
+		super();
+		// override parent default
+		pattern = DEFAULT_DATE_FORMAT_PATTERN;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setPageContext(PageContext pageContext) {
-        Cookie[] cookies = ((HttpServletRequest) pageContext.getRequest()).getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(GMT_COOKIE_NAME)) {
-                    offset = this.convertTimeZoneOffset(cookie.getValue());
-                    break;
-                }
-            }
-        }
-        super.setPageContext(pageContext);
-        this.setDateFormattingOptions();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		Cookie[] cookies = ((HttpServletRequest) pageContext.getRequest()).getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals(GMT_COOKIE_NAME)) {
+					offset = this.convertTimeZoneOffset(cookie.getValue());
+					break;
+				}
+			}
+		}
+		super.setPageContext(pageContext);
+		this.setDateFormattingOptions();
+	}
 
-    /**
-     * This method adds the corresponding offset to make the date and time
-     * match local user's timezone. If value is not a DateTime it's
-     * formatted as is
-     *
-     * @param value value to be formatted as date
-     * @throws JspTagException only from super() call
-     */
-    @Override
-    public void setValue(Object value) throws JspTagException {
-        if (value != null) {
-            DateTime time = (DateTime) value;
-            DateTimeZone zone = time.getZone();
-            long utcTime = zone.convertLocalToUTC(time.getMillis(), false);
-            time = new DateTime(utcTime + offset);
-            super.setValue(time);
-        } else {
-            super.setValue(null);
-        }
-    }
+	/**
+	 * This method adds the corresponding offset to make the date and time match local user's timezone. If value is not a DateTime it's formatted as is
+	 *
+	 * @param value
+	 *            value to be formatted as date
+	 * @throws JspTagException
+	 *             only from super() call
+	 */
+	@Override
+	public void setValue(Object value) throws JspTagException {
+		if (value != null) {
+			DateTime time = (DateTime) value;
+			DateTimeZone zone = time.getZone();
+			long utcTime = zone.convertLocalToUTC(time.getMillis(), false);
+			time = new DateTime(utcTime + offset);
+			super.setValue(time);
+		} else {
+			super.setValue(null);
+		}
+	}
 
-    /**
-     * Converts timezone offset representation to millisecond offset.
-     * If timezone offset representation is incorrect, then GMT
-     * timezone value is used.
-     *
-     * @param jsRepresentation time difference between GMT and
-     *                         local time, in minutes. Example: "-120"
-     * @return signed millisecond timezone offset
-     */
-    private long convertTimeZoneOffset(String jsRepresentation) {
-        try {
-            int offsetInMinutes = -Integer.parseInt(jsRepresentation);
-            return TimeUnit.MINUTES.toMillis(offsetInMinutes);
-        } catch (NumberFormatException e) {
-            // someone has passed wrong offset in cookie, use GMT
-            return DEFAULT_OFFSET;
-        }
-    }
+	/**
+	 * Converts timezone offset representation to millisecond offset. If timezone offset representation is incorrect, then GMT timezone value is used.
+	 *
+	 * @param jsRepresentation
+	 *            time difference between GMT and local time, in minutes. Example: "-120"
+	 * @return signed millisecond timezone offset
+	 */
+	private long convertTimeZoneOffset(String jsRepresentation) {
+		try {
+			int offsetInMinutes = -Integer.parseInt(jsRepresentation);
+			return TimeUnit.MINUTES.toMillis(offsetInMinutes);
+		} catch (NumberFormatException e) {
+			// someone has passed wrong offset in cookie, use GMT
+			return DEFAULT_OFFSET;
+		}
+	}
 
-    /**
-     * Figures out the the current locale and defines the date formatting pattern.
-     * Change the pattern here to affect all the dates formatting on pages.
-     */
-    private void setDateFormattingOptions() {
-        try {
-            HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
-            LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
-            this.setLocale(localeResolver.resolveLocale(request));
-        } catch (JspTagException e) {
-            throw new IllegalStateException("Error while rendering the date", e);
-        }
-    }
+	/**
+	 * Figures out the the current locale and defines the date formatting pattern. Change the pattern here to affect all the dates formatting on pages.
+	 */
+	private void setDateFormattingOptions() {
+		try {
+			this.setLocale(requestLocale());
+		} catch (JspTagException e) {
+			throw new IllegalStateException("Error while rendering the date", e);
+		}
+	}
+
+	private Locale requestLocale() {
+		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+		LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+		return localeResolver.resolveLocale(request);
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+		Locale lo = requestLocale();
+		if (Language.KOREAN.getLanguageCode().equals(lo.getLanguage())) {
+			pattern = DEFAULT_DATE_FORMAT_PATTERN_KOREAN;
+		}
+
+		return super.doEndTag();
+	}
 }
